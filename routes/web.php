0d78367a2f4d534d4c47264aac7bd70026bb5582
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-14 23:08:56 +0100

    add migrations

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('example');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
